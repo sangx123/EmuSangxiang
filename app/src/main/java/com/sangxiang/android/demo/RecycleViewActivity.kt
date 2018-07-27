10f34d61859d9b748efd73ae04a5dc31ebbb0f2a
桑享 <sangx@emucoo.com>
2018-07-27 17:13:23 +0800

    新增刷新和加载更多

--- app/src/main/java/com/sangxiang/android/demo/RecycleViewActivity.kt
+++ app/src/main/java/com/sangxiang/android/demo/RecycleViewActivity.kt
@@ -12,6 +12,7 @@ import com.sangxiang.android.R.id.mRecyclerView
 import com.sangxiang.android.network.EmucooApiRequest
 import com.sangxiang.android.network.model.ContactsResult
 import com.sangxiang.android.network.param.ContactsParam
+import com.sangxiang.android.utils.recycleView.CustomLoadMoreView
 import com.sangxiang.android.utils.recycleView.HorizontalDividerItemDecoration
 import com.squareup.picasso.Picasso
 import io.reactivex.Observer
@@ -22,9 +23,12 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_recycle_view.*
 import kotlinx.android.synthetic.main.item_organization_shop_user_select.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.support.v4.onRefresh
+import org.jetbrains.anko.toast
 import java.util.ArrayList
 
 class RecycleViewActivity :BaseActivity() {
+    private val PAGE_SIZE = 6
     lateinit var mAdapter:BaseQuickAdapter<ContactsResult.UserItem,BaseViewHolder>
     var mList:ArrayList<ContactsResult.UserItem>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,22 +39,49 @@ class RecycleViewActivity :BaseActivity() {
         })
         mAdapter=object :BaseQuickAdapter<ContactsResult.UserItem,BaseViewHolder>(R.layout.item_organization_shop_user_select,mList) {
             override fun convert(helper: BaseViewHolder, item: ContactsResult.UserItem) {
-//                mName.text=item.name
-//                if(!item.imgUrl.isNullOrBlank())
-//                Picasso.with().load(item.imgUrl).into(mImage)
                 helper.setText(R.id.mName,item.name)
-               // var text=helper.getView<TextView>(R.id.mName)
             }
 
         }
+
         var notDataView = layoutInflater.inflate(R.layout.empty_view, mRecyclerView.parent as ViewGroup, false)
         mAdapter.emptyView = notDataView
         notDataView.onClick {
             initData()
         }
+        mAdapter.setLoadMoreView(CustomLoadMoreView())
+        mAdapter.setOnLoadMoreListener { loadMore() }
         mRecyclerView.adapter=mAdapter
         mRecyclerView.addItemDecoration(HorizontalDividerItemDecoration.Builder(this).showLastDivider().build())
+        mSwipeRefreshLayout.onRefresh {
+            refresh()
+        }
+    }
 
+    private fun loadMore() {
+        isRefresh=false
+        initData()
+    }
+
+    private var mNextRequestPage = 1
+    private fun setData(isRefresh: Boolean, data: MutableList<ContactsResult.UserItem>) {
+        val size = data?.size
+        if (isRefresh) {
+            mNextRequestPage++
+            mAdapter.setNewData(data)
+        } else {
+            if (size > 0) {
+                mAdapter.addData(data)
+                mNextRequestPage++
+            }
+        }
+        if (size < PAGE_SIZE) {
+            //第一页如果不够一页就不显示没有更多数据布局
+            mAdapter.loadMoreEnd(isRefresh)
+            toast("no more data")
+        } else {
+            mAdapter.loadMoreComplete()
+        }
     }
 
     private fun initData() {
@@ -70,8 +101,12 @@ class RecycleViewActivity :BaseActivity() {
                     override fun onNext(t: ContactsResult) {
                         t?.let {
                             t.users?.let {
-                                mList= it as ArrayList<ContactsResult.UserItem>?
-                                mAdapter.setNewData(mList)
+                                it as ArrayList<ContactsResult.UserItem>?
+                                if(isRefresh){
+                                    mAdapter.setEnableLoadMore(true)
+                                    mSwipeRefreshLayout.isRefreshing = false
+                                }
+                                setData(isRefresh, it)
                             }
                         }
 
@@ -79,9 +114,24 @@ class RecycleViewActivity :BaseActivity() {
 
                     override fun onError(e: Throwable) {
                         e.printStackTrace()
+                        if(isRefresh){
+                            mAdapter.setEnableLoadMore(true)
+                            mSwipeRefreshLayout.isRefreshing = false
+                        }else{
+                            mAdapter.loadMoreFail()
+                        }
+
                     }
 
                 })
+    }
+
+    var isRefresh=true
+    private fun refresh() {
+        isRefresh=true
+        mNextRequestPage = 1
+        mAdapter.setEnableLoadMore(false)//这里的作用是防止下拉刷新的时候还可以上拉加载
+        initData()
     }
 
 }
